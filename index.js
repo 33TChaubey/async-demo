66c83326d40500ef7c2e85d9b92158e7549dda92
Trilokinath Chaubey <chaubeytn2103@outlook.com>
2024-03-29 21:04:52 +0530

    new exercisr

--- index.js
+++ index.js
@@ -1,61 +1,108 @@
-console.log('before');
-// getUser(1, (user) => {
-//    getRepository(user.gitHubUsername, (repos) =>{
-//     console.log('Repos', repos)
-//    });
-// });
+// console.log('before');
+// // getUser(1, (user) => {
+// //    getRepository(user.gitHubUsername, (repos) =>{
+// //     console.log('Repos', repos)
+// //    });
+// // });
 
 
-// getUser(1)
-// .then(user => getRepository(user.gitHubUsername))
-// .then(repos => getCommits(repos[0]))
-// .then(commits => console.log('Commits', commits))
-// .catch(err => console.log('Error', err.message))
+// // getUser(1)
+// // .then(user => getRepository(user.gitHubUsername))
+// // .then(repos => getCommits(repos[0]))
+// // .then(commits => console.log('Commits', commits))
+// // .catch(err => console.log('Error', err.message))
 
 
-// Async and Await Approach
-async function displayCommits() {
-    try{
-        const user = await getUser(1);
-    const repos = await getRepository(user.gitHubUsername);
-    const commits = await getCommits(repos[0]);
-    console.log(commits);
-    }
-    catch{
-        console.log('Error', err.message);
-    }
+// // Async and Await Approach
+// async function displayCommits() {
+//     try{
+//         const user = await getUser(1);
+//     const repos = await getRepository(user.gitHubUsername);
+//     const commits = await getCommits(repos[0]);
+//     console.log(commits);
+//     }
+//     catch{
+//         console.log('Error', err.message);
+//     }
     
-};
+// };
 
-displayCommits();
+// displayCommits();
 
-console.log('after');
+// console.log('after');
 
 
-function getUser(id){
+// function getUser(id){
+//     return new Promise((resolve, reject) =>{
+//         setTimeout(()=>{
+//             console.log('Reading the user from database');
+//             resolve({id:id, gitHubUsername:'33TChaubey'});
+//         }, 2000);
+//     })
+// }
+
+// function getRepository(username, callback){
+//     return new Promise((resolve, reject) =>{
+//         setTimeout(()=>{
+//             console.log('Reading the repository')
+//             reject(new Error('Could not get the repos'));
+//         }, 2000);
+//     });
+      
+// };
+
+// function getCommits(repo, callback){
+//     return new Promise((resolve, reject) =>{
+//         setTimeout(()=>{
+//             console.log('Calling Github API ...');
+//             resolve(['commit'])
+//         }, 2000)
+//     });
+// };
+
+async function notifyCustomer(){
+
+
+    const customer = await getCustomer(1);
+    console.log('Customer: ', customer);
+    if  (customer.isGold){
+        const movies = await getTopMoview();
+        console.log('Movies: ', movies);
+        await sendEmail(customer.email, movies);
+        console.log('Emailsent...');
+    }
+}
+notifyCustomer();
+
+function getCustomer(id){
     return new Promise((resolve, reject) =>{
         setTimeout(()=>{
-            console.log('Reading the user from database');
-            resolve({id:id, gitHubUsername:'33TChaubey'});
-        }, 2000);
+            resolve({
+                id:1,
+                name:"Trilokinath Chaubey",
+                email:"email.com",
+                isGold: true
+            });
+        },4000);
     })
+    
 }
 
-function getRepository(username, callback){
+function getTopMoview(){
     return new Promise((resolve, reject) =>{
-        setTimeout(()=>{
-            console.log('Reading the repository')
-            reject(new Error('Could not get the repos'));
-        }, 2000);
+        setTimeout(() =>{
+            resolve(['movie1', 'movie2'])
+        },4000);
     });
-      
-};
+    
+}
 
-function getCommits(repo, callback){
+
+function sendEmail(email, movies){
     return new Promise((resolve, reject) =>{
-        setTimeout(()=>{
-            console.log('Calling Github API ...');
-            resolve(['commit'])
-        }, 2000)
+        setTimeout(() =>{
+            resolve();
+        },4000);
     });
-};
+    
+}
